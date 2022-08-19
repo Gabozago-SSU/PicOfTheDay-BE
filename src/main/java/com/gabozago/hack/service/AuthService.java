90cf9addb9d7b5f8711fa5263fb3e1bb4a1e8131
thisIsJooS <thisis.joos@gmail.com>
2022-08-19 09:11:22 +0900

    구글로그인 properties 추가

--- src/main/java/com/gabozago/hack/service/AuthService.java
+++ src/main/java/com/gabozago/hack/service/AuthService.java
@@ -7,8 +7,6 @@ import com.gabozago.hack.dto.user.UserProfileImageDto;
 import com.gabozago.hack.repository.place.UserRepo;
 import lombok.RequiredArgsConstructor;
 
-import netscape.javascript.JSObject;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -30,7 +28,6 @@ import java.util.Map;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @Service
 @RequiredArgsConstructor
@@ -40,20 +37,20 @@ public class AuthService {
     private final UserRepo userRepository;
     private final S3Uploader s3Uploader;
 
-//    @Value("${GOOGLE_SNS_BASE_URL}")
-//    private String GOOGLE_SNS_BASE_URL;
-//
-//    @Value("${GOOGLE_SNS_CLIENT_ID}")
-//    private String GOOGLE_SNS_CLIENT_ID;
-//
-//    @Value("${GOOGLE_SNS_CALLBACK_URL}")
-//    private String GOOGLE_SNS_CALLBACK_URL;
-//
-//    @Value("${GOOGLE_SNS_CLIENT_SECRET}")
-//    private String GOOGLE_SNS_CLIENT_SECRET;
-//
-//    @Value("${GOOGLE_SNS_TOKEN_BASE_URL}")
-//    private String GOOGLE_SNS_TOKEN_BASE_URL;
+    @Value("${GOOGLE_SNS_BASE_URL}")
+    private String GOOGLE_SNS_BASE_URL;
+
+    @Value("${GOOGLE_SNS_CLIENT_ID}")
+    private String GOOGLE_SNS_CLIENT_ID;
+
+    @Value("${GOOGLE_SNS_CALLBACK_URL}")
+    private String GOOGLE_SNS_CALLBACK_URL;
+
+    @Value("${GOOGLE_SNS_CLIENT_SECRET}")
+    private String GOOGLE_SNS_CLIENT_SECRET;
+
+    @Value("${GOOGLE_SNS_TOKEN_BASE_URL}")
+    private String GOOGLE_SNS_TOKEN_BASE_URL;
 
     public ResponseEntity setNickname(UserNicknameDto userNicknameDto) {
         User user = userRepository.findById(userNicknameDto.getUserId())
@@ -64,62 +61,62 @@ public class AuthService {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-//    public String requestAccessToken(String code) {
-//        RestTemplate restTemplate = new RestTemplate();
-//
-//        Map<String, Object> params = new HashMap<>();
-//        params.put("code", code);
-//        params.put("client_id", GOOGLE_SNS_CLIENT_ID);
-//        params.put("client_secret", GOOGLE_SNS_CLIENT_SECRET);
-//        params.put("redirect_uri", GOOGLE_SNS_CALLBACK_URL);
-//        params.put("grant_type", "authorization_code");
-//
-//        ResponseEntity<String> responseEntity = restTemplate.postForEntity(GOOGLE_SNS_TOKEN_BASE_URL, params, String.class);
-//
-//        if (responseEntity.getStatusCode() != HttpStatus.OK) {
-//            return "구글 로그인 요청 실패";
-//        }
-//
-//        JSONObject jsonObject = new JSONObject(responseEntity.getBody());
-//        String access_token = jsonObject.getString("access_token");
-//        return access_token;
-//    }
-//
-//    public String getGoogleSnsId(String code) {
-//        String access_token = requestAccessToken(code);
-//        try {
-//            URL url = new URL("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + access_token);
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//            conn.setRequestMethod("GET");
-//            conn.setRequestProperty("Content-Type", "application/json");
-//            conn.setDoOutput(true);
-//
-//            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-//            StringBuilder sb = new StringBuilder();
-//            String line = null;
-//
-//            while ((line = br.readLine()) != null) {
-//                sb.append(line);
-//            }
-//
-//            JSONObject userInfo = new JSONObject(sb.toString());
-//            String snsId = userInfo.getString("id");
-//
-//            return snsId;
-//
-//        } catch (Exception e) {
-//            return "";
-//        }
-//    }
+    public String requestAccessToken(String code) {
+        RestTemplate restTemplate = new RestTemplate();
 
-//    public Long getUserIdByCode(String code) {
-//        String snsId = getGoogleSnsId(code);
-//        User user = userRepository.findBySnsId(snsId)
-//                .orElseThrow(() -> new IllegalStateException("그런 유저 없음"));
-//
-//        return user.getId();
-//    }
+        Map<String, Object> params = new HashMap<>();
+        params.put("code", code);
+        params.put("client_id", GOOGLE_SNS_CLIENT_ID);
+        params.put("client_secret", GOOGLE_SNS_CLIENT_SECRET);
+        params.put("redirect_uri", GOOGLE_SNS_CALLBACK_URL);
+        params.put("grant_type", "authorization_code");
+
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(GOOGLE_SNS_TOKEN_BASE_URL, params, String.class);
+
+        if (responseEntity.getStatusCode() != HttpStatus.OK) {
+            return "구글 로그인 요청 실패";
+        }
+
+        JSONObject jsonObject = new JSONObject(responseEntity.getBody());
+        String access_token = jsonObject.getString("access_token");
+        return access_token;
+    }
+
+    public String getGoogleSnsId(String code) {
+        String access_token = requestAccessToken(code);
+        try {
+            URL url = new URL("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + access_token);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            JSONObject userInfo = new JSONObject(sb.toString());
+            String snsId = userInfo.getString("id");
+
+            return snsId;
+
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public Long getUserIdByCode(String code) {
+        String snsId = getGoogleSnsId(code);
+        User user = userRepository.findBySnsId(snsId)
+                .orElseThrow(() -> new IllegalStateException("그런 유저 없음"));
+
+        return user.getId();
+    }
 
     public ResponseEntity setProfileImage(UserProfileImageDto userProfileImageDto, MultipartFile multipartFile) throws IOException {
 
